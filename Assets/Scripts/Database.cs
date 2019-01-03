bb1e84b342dc1ef1c33fa52a5b8380a09f7027ca
slavniyteo <slavniyteo@gmail.com>
2019-01-03 16:10:34 +0300

    First iteration

---
+++ Assets/Scripts/Database.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName="Database", menuName="Database")]
+public class Database : ScriptableObject
+{
+    [SerializeField] private List<Target> list = new List<Target>();
+
+    public Dictionary<KeyCode, Target> dict;
+
+    private void OnEnable()
+    {
+        dict = new Dictionary<KeyCode, Target>(list.Count);
+        foreach (var t in list)
+        {
+            dict[t.key] = t;
+        }
+    }
+}
+
+[Serializable]
+public class Target
+{
+    public KeyCode key;
+    public string text;
+    public Sprite picture;
+}
---
+++ Assets/Scripts/Input.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Input : MonoBehaviour
+{
+    public Image pic;
+    public Text letter;
+
+    public Database db;
+    
+    void OnGUI()
+    {
+        if (!UnityEngine.Input.anyKeyDown) return;
+        
+        var e = Event.current;
+        if (e.type != EventType.KeyDown) return;
+
+        ApplyKeyDown(e);
+    }
+
+    private void ApplyKeyDown(Event e)
+    {
+        Target t = null;
+        db.dict.TryGetValue(e.keyCode, out t);
+
+        if (t == null) return;
+
+        pic.sprite = t.picture;
+        letter.text = t.text;
+
+        Debug.Log($"Key {t.key} is pressed");
+    }
+}
